395da2269dc6aaaf7de8331c0765b82b2d4714bc
Ilya <kriminalmykriminal@gmail.com>
2019-10-26 12:45:39 +0300

    portfolio added

--- notesApp/src/components/Loader.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export const Loader = () => (
-  <div className="text-center">
-    <div className="spinner-border" role="status">
-      <span className="sr-only">Loading...</span>
-    </div>
-  </div>
-)
